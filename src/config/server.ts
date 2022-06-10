443c845ebe626c6e5e8b344ceddab2f6017cc5fc
facu410 <facundito410@gmail.com>
2022-06-10 20:15:39 -0300

    test2

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,7 +3,7 @@ const app = express();
 const PORT: string | number = process.env.PORT || 5000;
 
 app.use("*", (req, res) => {
-  res.send("<h1>Welcome to your simple server! Awesome ri,ght</h1>");
+  res.send("<h1>Welcome to your simple server! Awesome right</h1>");
 });
 
 app.listen(PORT, () => console.log(`hosting @${PORT}`));
